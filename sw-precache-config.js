fc4c4ea64ad589fcf5fe1056a7ced8060293967f
Nicholas Roberts <nicholas.roberts.au@gmail.com>
2016-12-27 18:16:29 +1100

    multiple small tweaks

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -11,6 +11,8 @@ module.exports = {
   staticFileGlobs: [
     '/index.html',
     '/manifest.json',
+    '/data/resume.json',
+    '/data/portfolio.json',
     '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
   ],
   navigateFallback: '/index.html'
