cd88035355ca20d4a0ed2a7fc019054c64688bb3
Nicholas Roberts <nicholas.roberts.au@gmail.com>
2017-05-26 12:46:13 +1000

    Corrected polymer build config

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -13,7 +13,7 @@ module.exports = {
     '/manifest.json',
     '/data/resume.json',
     '/data/portfolio.json',
-    '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
+    '/bower_components/webcomponentsjs/webcomponents-loader.js'
   ],
   navigateFallback: '/index.html'
 };
